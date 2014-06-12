92d486e453d574e01a4091ad51a7ecec0c571b72
Drew Cuthbertson <syeoryn@gmail.com>
2014-06-11 17:14:47 -0700

    Adds fixture data

--- js/models/todo.js
+++ js/models/todo.js
@@ -2,3 +2,21 @@ Todos.Todo = DS.Model.extend({
   title: DS.attr('string'),
   isCompleted: DS.attr('boolean')
 });
+
+Todos.Todo.FIXTURES = [
+  {
+    id: 1,
+    title: 'Study JavaScript',
+    isCompleted: true
+  },
+  {
+    id: 2,
+    title: 'Study more JavaScript',
+    isCompleted: false
+  },
+  {
+    id: 3,
+    title: 'Study even more Javascript',
+    isCompleted: false
+  }
+]
